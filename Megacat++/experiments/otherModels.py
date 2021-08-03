c8032eb5e3cc7e3bd2e0c095c72d84ab5936c370
Milk <mastermilkx@gmail.com>
2021-08-03 13:51:32 -0400

    trained baseline models for domains

--- Megacat++/experiments/otherModels.py
+++ Megacat++/experiments/otherModels.py
@@ -6,6 +6,9 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 import sys
 from keras.preprocessing.sequence import TimeseriesGenerator
+import json
+import random
+
 
 
 '''
@@ -16,6 +19,7 @@ from keras.preprocessing.sequence import TimeseriesGenerator
 
 #converts sequence to the baseline format for the simple lstm model
 # returns train x and y sequence subsets and test sequence
+'''
 def seq2BaseData(seq,look_back):
 	generator = TimeseriesGenerator(seq, seq, length=look_back, batch_size=1)
 	X = []
@@ -31,6 +35,36 @@ def seq2BaseData(seq,look_back):
 			tX.append(xi)
 
 	return X,y, tX
+'''
+
+#reapply the answer to the missing ? in the sequence and create train and test data
+def createBaseTraining(questions, look_back):
+	#add answer back into sequence
+	full_seqs = []
+	for d in questions:
+		s = d["sequence"].copy()
+		s[np.where(np.array(s) == "?")[0][0]] = d["options"][int(d["answer"])]	#replace ?
+		s = list(map(lambda x: int(x),s))							#convert seq to int format
+		full_seqs.append(s)
+
+
+	#create generator sequences from the complete sequences
+	X = []
+	y = []
+
+	for seq in full_seqs:
+	    train_gen = TimeseriesGenerator(seq, seq, length=look_back, batch_size=1)
+	    for i in range(len(train_gen)):
+	        xi, yi = train_gen[i]
+	        X.append(xi)
+	        y.append(yi)
+
+	#clean up shape
+	X = np.squeeze(np.asarray(X))
+	y = np.squeeze(np.asarray(y))
+	X = X.reshape((X.shape[0],look_back,1))
+
+	return X, y
 
 
 '''
@@ -38,7 +72,14 @@ def seq2BaseData(seq,look_back):
 	MODELS
 
 '''
-
+#import the json file that holds the domain data
+# json format: "library": [rep for index numbers], "questions": [set of questions ]
+# question format: "sequence": integer sequence with a single '?' character, "options": multiple choice integer options (respond according to the index of the answer), "answer": index value of the answer from the option choices, "id": id no of the question
+def importJSON(jfile):
+	dat = {}
+	with open(jfile, "r") as f:
+		dat = json.load(f)
+	return dat
 
 #baseline model with simple RNN with one LSTM layer
 def makeBaseModel(input_shape):
@@ -47,3 +88,45 @@ def makeBaseModel(input_shape):
 	model.add(Dense(1))
 	model.compile(optimizer='adam', loss='mse',metrics=['mse'])
 	return model
+
+def trainBaseModel(input_shape, domain_name, domain_questions):
+	#make the model
+	print("-- Creating model --")
+	base_model = makeBaseModel(input_shape)
+
+	#create training data
+	print("-- Generating training data --")
+	X_train, y_train = createBaseTraining(domain_questions, input_shape)
+
+	#train
+	print(f"-- Training model on {len(X_train)} samples--")
+	history = base_model.fit(X_train, y_train, epochs=1000, validation_split=0.2, verbose=1)
+
+	#quick test
+	print("-- TEST -- ")
+	r = random.randrange(len(X_train))
+	x_test = X_train[r:r+2]
+	y_test = y_train[r:r+2]
+	p = np.squeeze(base_model.predict(x_test))
+	print(f"Actual: {y_test}")
+	print(f"Prediction: {p}")
+
+	#export it
+	print("-- Exporting model --")
+	base_model.save(f"base_models/base_{domain_name}_model.h5")
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("MUST SPECIFY DOMAIN TO TRAIN BASE MODEL ON!")
+		exit(1)
+
+	domain = sys.argv[1]
+	fib_look = int(sys.argv[2]) if len(sys.argv) > 2 else 3
+	trainBaseModel(fib_look,domain, importJSON(f"question_set/{domain}_train.json")["questions"])
+
+
+
+
+
+
--- Megacat++/experiments/run_exp.py
+++ Megacat++/experiments/run_exp.py
@@ -10,15 +10,54 @@ import argparse
 import os
 from tqdm import tqdm
 
+#import bots
+from iq_bot import predictSeq
+from 
+
 
 #import the json file that holds the domain data
 # json format: "library": [rep for index numbers], "questions": [set of questions ]
 # question format: "sequence": integer sequence with a single '?' character, "options": multiple choice integer options (respond according to the index of the answer), "answer": index value of the answer from the option choices, "id": id no of the question
 def importJSON(jfile):
-	return
+	dat = {}
+	with open(jfile, "r") as f:
+		dat = json.load(f)
+	return dat
 
 
-def run_exp():
+#runs the experiment on the domain questions and returns accuracy, error, and other data
+def run_exp(domain, test_type, bot):
+	#import dataset questions
+	domain_dat = importJSON(f"question_set/{domain}_{test_type}.json")
+	domain_key = domain_dat['library']
+	domain_quest = domain_dat['questions']
+
+	#set up accuracies and other stored information
+	correct = 0
+	incorrect = 0
+	raw_ans = []
+
+	#specific to megacat bot (model type info)
+	megacat_model_ans = {"hybrid":0, "index":0, "recursive":0}
+	megacat_model_correct = {"hybrid":0, "index":0, "recursive":0}
+
+	#import the baseline bot
+	if bot == "base":
+
+
+	#iterate through every questions
+	for q in domain_quest:
+		resp = -1
+		#random bot = select random option choice
+		if bot == "random":
+			resp = random.range(len(q['options']))
+
+		#get baseline bot response
+		elif bot == "base":
+
+
+
+
 	return
 
 
@@ -28,10 +67,26 @@ if __name__ == "__main__":
 	#parse arguments if any
 	parser = argparse.ArgumentParser(description='Run bots on different domains of sequences')
 	parser.add_argument('-b', '--bot',type=str,dest='b', help='Bot to use for experiment [ALL, MEGACAT, BASE, RANDOM]',required=True)
-	parser.add_argument('-d', '--domain',type=str,dest='d', help='which domain to use [MARIO, SONG, CELL]',required=True)
+	parser.add_argument('-d', '--domain',type=str,dest='d', help='which domain to use [MARIO, SONG, CELL, ARITH]',required=True)
 	parser.add_argument('-t', '--type',type=str,dest='t', help='which type of test to make [BOTH, TRAIN, TEST]',required=False)
 	args = parser.parse_args()
 
+	#run all/one of the bots
+	bots = []
+	if args.b.upper() == "ALL":
+		bots = ["random", "base", "megacat"]
+	else:
+		bots = [args.b.lower()]
 
+	#run both/either test and training set
+	tt = []
+	if args.t.upper() == "BOTH":
+		tt = ["train", "test"]
+	else
+		tt = [args.t.lower()]
 
-	run_exp()
+	#run specific bots and test types on the domain data seqeunces
+	for b in bots:
+		for t in tt:
+			run_exp(args.d, t, b)
+
